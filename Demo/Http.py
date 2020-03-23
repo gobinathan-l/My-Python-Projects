eeed4d9d965445cea96e8bc55ecf0d9624fa5b80
gobinathan-l <gobinathan.contact@gmail.com>
2020-03-23 15:22:17 +0530

    Update

--- Demo/Http.py
+++
@@ -1,7 +0,0 @@
-import requests
-url = "https://icanhazdadjoke.com/"
-response = requests.get(url, headers={"Accept":"application/json"})
-content = response.json()
-
-print(content['joke'])
-print(f"Status: {content['status']}")
